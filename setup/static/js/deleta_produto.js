4a53ee367248b3770ed6eb32729134065edc9cef
joedsoncarvalho <joedson.silva@laclaw.com.br>
2022-09-28 12:48:40 -0300

    Modo de deletar antigo

--- setup/static/js/deleta_produto.js
+++ setup/static/js/deleta_produto.js
@@ -3,65 +3,29 @@ var dataurl = document.URL;
 var splitbarra = dataurl.split("/");
 var idURL = splitbarra.find(item => item > 0);
 
-// let produtoQueSeraDeletado = idURL;
+let produtoQueSeraDeletado = idURL;
 
-// let myHeaders = new Headers();
+let myHeaders = new Headers();
 
-// myHeaders.append('Authorization', `Basic ${btoa('Publico:usuariopublico')}`)
-
-
-// let requestOptions = {
-//     method: 'DELETE', 
-//     headers: myHeaders,
-//     redirect: 'follow'
-// }
-// let loading = document.getElementById('principal')
-// loading.style.display = 'block'
-
-// fetch(`http://18.231.157.213/api/products/${produtoQueSeraDeletado}/`, requestOptions)
-// .then((result) => {
-//     console.log(result)
-//     console.log('Produto deletado.')
-//     window.location.href = '/produtos';
-// })
-// .catch((error) => console.log(error))
-// .finally(() =>{
-//     loading.style.display = 'none' 
-// })
-
-// let produtoQueSeraDeletado = idURL;
-
-// let myHeaders = new Headers();
-
-// myHeaders.append('Authorization', `Basic ${btoa('Publico:usuariopublico')}`)
-// let formData = new FormData();
-// formData.append('discontinued', '1')
+myHeaders.append('Authorization', `Basic ${btoa('Publico:usuariopublico')}`)
 
 
-// let requestOptions = {
-//     method: 'PUT', 
-//     headers: myHeaders,
-//     body: formData,
-//     redirect: 'follow'
-// }
-// let loading = document.getElementById('principal')
-// loading.style.display = 'block'
+let requestOptions = {
+    method: 'DELETE', 
+    headers: myHeaders,
+    redirect: 'follow'
+}
+let loading = document.getElementById('principal')
+loading.style.display = 'block'
 
-// fetch(`http://18.231.157.213/api/products/${produtoQueSeraDeletado}/`, requestOptions)
-// .then((result) => {
-//     console.log(result)
-//     console.log('Produto deletado.')
-//     window.location.href = '/produtos';
-// })
-// .catch((error) => console.log(error))
-// .finally(() =>{
-//     loading.style.display = 'none' 
-// })""
-
-
-
-
-
-
-
+fetch(`http://18.231.157.213/api/products/${produtoQueSeraDeletado}/`, requestOptions)
+.then((result) => {
+    console.log(result)
+    console.log('Produto deletado.')
+    window.location.href = '/produtos';
+})
+.catch((error) => console.log(error))
+.finally(() =>{
+    loading.style.display = 'none' 
+})
 
